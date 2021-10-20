31e4a90e416705e6a9dd2df9b9dd53f5569a55e5
uzairriaz <uzairriaz.pk@gmail.com>
2021-10-20 12:31:27 +0500

    Initial Commit

---
+++ honeymoon/honeymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  honeymoonApp.swift
+//  honeymoon
+//
+//  Created by Umair Riaz on 20/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct honeymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
